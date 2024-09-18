65f7c550f2ba34cf9790283e867b817f4d9b77b3
Eduardo Angel Tovar Ortega <angel2004tovar@gmail.com>
2024-09-17 19:03:57 -0600

    Creación del metodo para modificar

--- Manejador/ManejadorProductos.cs
+++ Manejador/ManejadorProductos.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AccesoDatos;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
 
 namespace Manejador
 {
@@ -16,6 +17,21 @@ namespace Manejador
         {
             MessageBox.Show(f.Guardar($"CALL p_InsertarProductos( '{Nombre.Text}', '{Descripcion.Text}', {Precio.Text})"),
                 "!Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void ModificarProductos(TextBox Nombre, TextBox Descripcion, TextBox Precio, int id)
+        {
+            MessageBox.Show(f.Modificar($"CALL p_ModificarProductos({id}, '{Nombre.Text}', '{Descripcion.Text}', {Precio.Text})"),
+                "!Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        DataGridViewButtonColumn Boton(string t, Color co)
+        {
+            DataGridViewButtonColumn bo = new DataGridViewButtonColumn();
+            bo.Text = t;
+            bo.UseColumnTextForButtonValue = true;
+            bo.FlatStyle = FlatStyle.Popup;
+            bo.DefaultCellStyle.BackColor = co;
+            bo.DefaultCellStyle.ForeColor = Color.White;
+            return bo;
         }
     }
 }
